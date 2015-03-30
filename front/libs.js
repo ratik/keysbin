dcb4fb23199ce0a650fb3125d4e3aa01d083646a
Sergey Ratiashvili <serg.s.r@gmail.com>
2015-03-30 05:01:05 +0000

    moved some libs

--- front/libs.js
+++ front/libs.js
@@ -7,3 +7,5 @@ require('angular-ui-router');
 require('thirty-two');
 require('qr-image');
 require('crypto-js');
+require('async');
+require('angular-bootstrap-nav-tree/dist/abn_tree_directive.js');
